82af2e7186c60d0cd19841d0c48cacd67e62ad0e
LuisRincon16 <lurinconj@unal.edu.co>
2024-12-15 05:37:43 +0000

    int21

--- zooAnimales/animal.py
+++ zooAnimales/animal.py
@@ -55,7 +55,7 @@ class Animal:
         from zooAnimales.reptil import Reptil
         from zooAnimales.pez import Pez
         from zooAnimales.anfibio import Anfibio
-        return (f"Mamiferos: {len(Mamifero.getListado())}\n" f"Aves: {len(Ave.getListado())}\n" f"Reptiles: {len(Reptil.getListado())}\n" f"Peces: {len(Pez.getListado())}\n" f"Anfibios: {len(Anfibio.getListado())}")
+        return (f"Mamiferos : {len(Mamifero.getListado())}\n" f"Aves : {len(Ave.getListado())}\n" f"Reptiles : {len(Reptil.getListado())}\n" f"Peces : {len(Pez.getListado())}\n" f"Anfibios : {len(Anfibio.getListado())}")
     
     def toString(self):
         if self.getZona() is None:
